c2937f044f173529ff2c22a8357e25579c21045d
supatomic <kngoyal.cs@gmail.com>
2021-06-08 16:55:17 -0400

    serialize and de-serialize block objects

---
+++ part-3/blockchain/block.go
@@ -0,0 +1,52 @@
+package blockchain
+
+import (
+	"bytes"
+	"encoding/gob"
+	"log"
+)
+
+type Block struct {
+	Hash     []byte
+	Data     []byte
+	PrevHash []byte
+	Nonce    int
+}
+
+func CreateBlock(data string, prevHash []byte) *Block {
+	block := &Block{[]byte{}, []byte(data), prevHash, 0}
+	pow := NewProof(block)
+	block.Nonce, block.Hash = pow.Run()
+	return block
+}
+
+func Genesis() *Block {
+	return CreateBlock("Genesis", []byte{})
+}
+
+func (b *Block) Serialize() []byte {
+	var res bytes.Buffer
+	encoder := gob.NewEncoder(&res)
+
+	err := encoder.Encode(b)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return res.Bytes()
+}
+
+func Deserialize(data []byte) *Block {
+	var block Block
+
+	decoder := gob.NewDecoder(bytes.NewReader(data))
+
+	err := decoder.Decode(&block)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return &block
+}
