2a8a194c7cb918ac7dbb753b2bc22524647a5c38
supatomic <kngoyal.cs@gmail.com>
2021-06-08 16:17:47 -0400

    a simple proof of work to find the respective nonce

--- part-2/main.go
+++ part-2/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/golang-blockchain/part-2/blockchain"
 )
@@ -16,6 +17,9 @@ func main() {
 	for _, block := range chain.Blocks {
 		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
 		fmt.Printf("Data in Block: %s\n", block.Data)
-		fmt.Printf("Hash: %x\n\n", block.Hash)
+		fmt.Printf("Hash: %x\n", block.Hash)
+
+		pow := blockchain.NewProof(block)
+		fmt.Printf("POW: %s\n\n", strconv.FormatBool(pow.Validate()))
 	}
 }
