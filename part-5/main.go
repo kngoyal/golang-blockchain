8f6ccda095fdc52c2568c45dd157f9673b1ada9b
supatomic <kngoyal.cs@gmail.com>
2021-06-12 21:32:15 -0400

    add custome logging levels

--- part-5/main.go
+++ part-5/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"os"
 
+	"github.com/golang-blockchain/part-5/logger"
 	"github.com/golang-blockchain/part-5/cli"
 )
 
 func main() {
+	logger.SetLogLevel()
 	defer os.Exit(0)
 	cmd := cli.CommandLine{}
 	cmd.Run()
